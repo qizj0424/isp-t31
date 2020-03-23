ab8b7c50fbf5bd261c28fd3dbd5ec4b98b77ca53
zjqi <1696808449@qq.com>
2020-03-23 17:59:38 +0800

    Low light optimization

--- trans.sh
+++ trans.sh
@@ -6,9 +6,9 @@ then
     echo "mounted"
 else
     echo "not mounted"
-    sudo mount -o nolock -t nfs 193.169.4.2:/home_b/nfswork/zjqi ~/nfswork/mnt
+    mount -o nolock -t nfs 193.169.4.2:/home_b/nfsroot/zjqi ~/nfswork/mnt
 fi
 
 make clean && make S=${SENSOR}
-sudo cp ${SENSOR}.bin ~/nfswork/mnt/ -vf                                                                                                   
+cp ${SENSOR}.bin ~/nfswork/mnt/ -vf                                                                                                   
 md5sum ${SENSOR}.bin
