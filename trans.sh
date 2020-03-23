3fedd86d48d27ff09a3ba0b458ab78df70b9f4c5
zjqi <1696808449@qq.com>
2020-03-23 18:00:45 +0800

    optimization trans.sh

--- trans.sh
+++ trans.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 SENSOR="sc3235-t31"
 
-sudo mount -o nolock -t nfs 193.169.4.2:/home_b/nfswork/zjqi ~/nfswork/mnt
+if mountpoint -q ~/nfswork/mnt
+then
+    echo "mounted"
+else
+    echo "not mounted"
+    mount -o nolock -t nfs 193.169.4.2:/home_b/nfsroot/zjqi ~/nfswork/mnt
+fi
+
 make clean && make S=${SENSOR}
-sudo cp ${SENSOR}.bin ~/nfswork/mnt/ -vf                                                                                                   
+cp ${SENSOR}.bin ~/nfswork/mnt/ -vf                                                                                                   
 md5sum ${SENSOR}.bin
