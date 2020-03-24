702b00572dfc5d32fbb0279b513256ca66a5d87d
zjqi <1696808449@qq.com>
2020-03-24 12:08:54 +0800

    change dgain is 1x,and blc is low

--- trans.sh
+++ trans.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
-sudo mount -o nolock -t nfs 193.169.4.2:/home_b/nfsroot/zjqi ~/nfswork/mnt
-make clean && make S=sc2310-t31
-sudo cp sc2310-t31.bin ~/nfswork/mnt/ -vf
-md5sum sc2310-t31.bin
+SENSOR="sc2310-t31"
+
+if mountpoint -q ~/nfswork/mnt
+then
+    echo "mounted"
+else
+    echo "not mounted"
+    mount -o nolock -t nfs 193.169.4.2:/home_b/nfsroot/zjqi ~/nfswork/mnt
+fi
+
+make clean && make S=${SENSOR}
+cp ${SENSOR}.bin ~/nfswork/mnt/ -vf                                                                                                   
+md5sum ${SENSOR}.bin
