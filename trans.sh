59a79b1b8748581d249e8964b73fcf6b03eb91ec
zjqi <1696808449@qq.com>
2020-04-01 17:55:19 +0800

    static is ok

--- trans.sh
+++ trans.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 SENSOR="gc2053-t31"
 
-if mountpoint -q ~/nfswork/mnt
-then
-    echo "mounted"
-else
-    echo "not mounted"
-    mount -o nolock -t nfs 193.169.4.2:/home_b/nfsroot/zjqi ~/nfswork/mnt
-fi
+#if mountpoint -q ~/nfswork/mnt
+#then
+#    echo "mounted"
+#else
+#    echo "not mounted"
+#    mount -o nolock -t nfs 193.169.4.2:/home_b/nfsroot/zjqi ~/nfswork/mnt
+#fi
 
 make clean && make S=${SENSOR}
-cp ${SENSOR}.bin ~/nfswork/mnt/ -vf                                                                                                   
+cp ${SENSOR}.bin /home/book/tftpboot/ -vf                                                                                                   
 md5sum ${SENSOR}.bin
