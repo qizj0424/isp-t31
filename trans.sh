17d3bbfe1a72bdf6aa684428ad54b5ebd55b6df0
zjqi <1696808449@qq.com>
2020-05-07 10:21:00 +0800

    trans.sh 2.1

--- trans.sh
+++ trans.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+####Select the sensor####
 SENSOR="sc3235-t31"
+#####NFS OR TFTP##Choose the upload method#
 TOOL="NFS"
 NFS_MY_PATH=/home/book/nfswork/mnt
 NFS_WORK_PATH=/home/zjqi/net/nfsroot
